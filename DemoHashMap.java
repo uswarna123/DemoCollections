eed0e93a76498f5c04f04bf8d7a3cfab5d5732c9
uswarna123 <109545573+uswarna123@users.noreply.github.com>
2024-02-19 23:25:57 -0500

    HashMap

--- DemoHashMap.java
+++ DemoHashMap.java
@@ -1,7 +1,17 @@
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class DemoHashMap {
-    Map map=new HashMap<>();
+    public static void main(String[] args) {
+        Map<Integer, String> map=new HashMap<>();
+        map.put(7,"Tom");
+        map.put(8,"cris");
+        map.put(9,"shawn");
+        System.out.println(map);
+        for (Map.Entry entry:map.entrySet()){
+            System.out.println(entry.getKey()+" "+entry.getValue());
+        }
 
+    }
 }
