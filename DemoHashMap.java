94b5938d6d21e7a6111bd9990b12423fdf61c6df
uswarna123 <109545573+uswarna123@users.noreply.github.com>
2024-02-19 23:38:19 -0500

    HashMap

--- DemoHashMap.java
+++ DemoHashMap.java
@@ -5,13 +5,20 @@ import java.util.Map;
 public class DemoHashMap {
     public static void main(String[] args) {
         Map<Integer, String> map=new HashMap<>();
+        Map<Integer, String> map2=new HashMap<>();
         map.put(7,"Tom");
         map.put(8,"cris");
         map.put(9,"shawn");
-        System.out.println(map);
+        map2.put(10,"shane");
+        System.out.println("first map: "+map);
+       map2.putAll(map);
+       // System.out.println(map2.putAll(map));
+        System.out.println("second map: "+map2);
         for (Map.Entry entry:map.entrySet()){
-            System.out.println(entry.getKey()+" "+entry.getValue());
+            System.out.println("Key: "+entry.getKey()+" Value: "+entry.getValue());
         }
+        System.out.println("number of keys in map1 "+map.size());
+        System.out.println("number of keys in map2 "+map2.size());
 
     }
 }
