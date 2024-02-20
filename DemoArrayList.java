cb31e6d551f0c735cfc82fd134aa94b8e1670efa
uswarna123 <109545573+uswarna123@users.noreply.github.com>
2024-02-19 22:38:21 -0500

    ArrayList

--- DemoArrayList.java
+++ DemoArrayList.java
@@ -4,19 +4,20 @@ import java.util.Scanner;
 public class DemoArrayList {
     public static void main(String[] args) {
         ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.add(0,"red");
-        arrayList.add(1,"pink");
-        arrayList.add(2,"blue");
+        arrayList.add("red");
+        arrayList.add("pink");
+        arrayList.add("blue");
         Scanner scanner=new Scanner(System.in);
        System.out.println("please enter a Number");
         int input= scanner.nextInt();
+        arrayList.add(0,String.valueOf(input));
         System.out.println("please enter a String");
         String string=scanner.next();
-        arrayList.add(String.valueOf(input));
         arrayList.add(string);
-        arrayList.remove(3);
         for (String lists:arrayList) System.out.println(lists);
+        arrayList.remove(0);
         System.out.println("Arraylist elements: "+arrayList +" size: "+ arrayList.size());
+        System.out.println("Getting specific element in an array by index : "+arrayList.get(0));
     }
 
 }
