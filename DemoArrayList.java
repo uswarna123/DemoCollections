e961f4afda1a700dcbd1ebc859bb98f62b3a1774
uswarna123 <109545573+uswarna123@users.noreply.github.com>
2024-02-20 22:33:02 -0500

    ArrayList

--- DemoArrayList.java
+++ DemoArrayList.java
@@ -1,23 +1,29 @@
 import java.util.ArrayList;
-import java.util.Scanner;
-
+import java.util.List;
 public class DemoArrayList {
-    public static void main(String[] args) {
-        ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.add("red");
-        arrayList.add("pink");
-        arrayList.add("blue");
-        Scanner scanner=new Scanner(System.in);
-       System.out.println("please enter a Number");
-        int input= scanner.nextInt();
-        arrayList.add(0,String.valueOf(input));
-        System.out.println("please enter a String");
-        String string=scanner.next();
+    static List<String> arrayList = new ArrayList<>();
+    public static void AddElementsToArrayList(String string)
+    {
         arrayList.add(string);
-        for (String lists:arrayList) System.out.println(lists);
-        arrayList.remove(0);
-        System.out.println("Arraylist elements: "+arrayList +" size: "+ arrayList.size());
-        System.out.println("Getting specific element in an array by index : "+arrayList.get(0));
+    }
+    public static void AddElementsToArrayList(Integer index,String string)
+    {
+        arrayList.add(index,string);
+    }
+    public static void RemoveElementByIndex(String index)
+    {
+        arrayList.remove(index);
+    }
+    public static void main(String[] args) {
+
+        DemoArrayList.AddElementsToArrayList("blue");
+        DemoArrayList.AddElementsToArrayList(0,"black");
+        System.out.println(arrayList);
+        DemoArrayList.AddElementsToArrayList(0,"pink");
+        System.out.println(arrayList);
+       // DemoArrayList.RemoveElementByIndex(String.valueOf(0));
+        DemoArrayList.arrayList.remove(0);
+        System.out.println(arrayList);
     }
 
 }
