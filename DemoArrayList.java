f730b21e5aeb62f192af74c2c49dad4de0fae858
uswarna123 <109545573+uswarna123@users.noreply.github.com>
2024-02-20 22:43:04 -0500

    Merge remote-tracking branch 'origin/Collections' into Collections
    # Conflicts:
    #	DemoArrayList.java

--- DemoArrayList.java
+++ DemoArrayList.java
@@ -1,28 +1,29 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
 public class DemoArrayList {
     static List<String> arrayList = new ArrayList<>();
-    public static void AddElementsToArrayList(String string)
+    public static void addElementsToArrayList(String string)
     {
         arrayList.add(string);
     }
-    public static void AddIndexAndValueToArrayList(Integer index,String string)
+    public static void addIndexAndValueToArrayList(int index,String string)
     {
         arrayList.add(index,string);
     }
-    public static void RemoveElementByIndex(String index)
+    public static void removeElementByIndex(int index)
     {
         arrayList.remove(index);
     }
     public static void main(String[] args) {
 
-        DemoArrayList.AddElementsToArrayList("blue");
-        DemoArrayList.AddIndexAndValueToArrayList(0,"black");
+        DemoArrayList.addElementsToArrayList("blue");
+        DemoArrayList.addIndexAndValueToArrayList(0,"black");
         System.out.println(arrayList);
-        DemoArrayList.AddIndexAndValueToArrayList(0,"pink");
+        DemoArrayList.addIndexAndValueToArrayList(0,"pink");
         System.out.println(arrayList);
-       // DemoArrayList.RemoveElementByIndex(String.valueOf(0));
-        DemoArrayList.arrayList.remove(0);
+        DemoArrayList.removeElementByIndex(2);
         System.out.println(arrayList);
     }
 
