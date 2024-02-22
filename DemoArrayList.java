ace39374cc1cee6f622ae4438007e12b02c716a9
uswarna123 <109545573+uswarna123@users.noreply.github.com>
2024-02-22 18:19:32 -0500

    Merge remote-tracking branch 'origin/Collections' into Collections
    # Conflicts:
    #	DemoArrayList.java

--- DemoArrayList.java
+++ DemoArrayList.java
@@ -1,37 +1,57 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-public class DemoArrayList {
+public class DemoArrayList  {
     static List<String> arrayList = new ArrayList<>();
-    public static void addElementsToArrayList(String string)
-    {
+    static List<String> arrayList1= new ArrayList<>();
+    static List<String> arrayList2= new ArrayList<>();
+        public static void setElementToArrayList (String string){
         arrayList.add(string);
-    }
-    public static void addIndexAndValueToArrayList(int index,String string)
-    {
-        arrayList.add(index,string);
-    }
-    public static void removeElementByIndex(int index)
-    {
-        arrayList.remove(index);
-    }
-    public static void iterateAStringThroughForLoop() {
+        }
+        public static void setElementsToArrayList (){
+        arrayList1.addAll(Arrays.asList("Max","Peter","Jackson","Alicia","Julien"));}
+    public static void setElementsToAnotherArrayList (){
+        arrayList2.addAll(Arrays.asList("Max","Peter","Jackson","Alicia","Julien"));}
+        public static void setIndexAndValueToArrayList ( int index, String string)
+        {
+            arrayList.add(index, string);
+        }
+        public static void getArrayList (int index)
+        {
+            System.out.print("inside getMethod :");
+            System.out.println(arrayList.get(index));
+        }
+        public static void removeElementByIndex ( int index)
+        {
+            arrayList.remove(index);
+        }
+        public static void getArrayListThroughForLoop () {
         for (Object string : arrayList.toArray()) {
             System.out.println(string);
         }
-
     }
     public static void main(String[] args) {
 
-        DemoArrayList.addElementsToArrayList("blue");
-        DemoArrayList.addIndexAndValueToArrayList(0,"black");
+        DemoArrayList.setElementToArrayList("blue");
+        DemoArrayList.setIndexAndValueToArrayList(0,"black");
         System.out.println(arrayList);
-        DemoArrayList.addIndexAndValueToArrayList(0,"pink");
-        DemoArrayList.addIndexAndValueToArrayList(0,"green");
-        DemoArrayList.addElementsToArrayList("red");
+        DemoArrayList.setIndexAndValueToArrayList(0,"pink");
+        DemoArrayList.setIndexAndValueToArrayList(0,"green");
+        DemoArrayList.setElementToArrayList("red");
         System.out.println(arrayList);
         DemoArrayList.removeElementByIndex(2);
         System.out.println(arrayList);
-        DemoArrayList.iterateAStringThroughForLoop();
+        DemoArrayList.getArrayListThroughForLoop();
+        DemoArrayList.getArrayList(3);
+        DemoArrayList.setElementsToArrayList();
+        DemoArrayList.setElementsToAnotherArrayList();
+        System.out.println("first array "+arrayList1);
+        System.out.println("second array "+arrayList2);
+        //List<Boolean> equals= Collections.singletonList(arrayList1.equals(arrayList2));
+        System.out.println(arrayList1.equals(arrayList2));
+        System.out.println(arrayList1.addAll(Arrays.asList("Json","Patricia"))+""+arrayList1);
+        System.out.println("first array "+arrayList1);
+        System.out.println("second array "+arrayList2);
     }
-
 }
